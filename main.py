d495906120b302df79a6590f4bfa057dc6bfd90d
Blitz <55319277+Blitzkriegs03@users.noreply.github.com>
2020-11-24 17:51:13 +0000

    playing arround with some code
    messing arround with discords api and working out how works. also a little bit of codinng on a module to interperate time format

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print('hello world')
+import discord
+from discord.ext import commands
+
+from decouple import config
+import datetime
+
+client = commands.Bot(command_prefix = '%')
+
+
+###############################################################################
+def get_datetime(str):
+    """
+    converts YYMMWWDD HHMMSS to datetime
+    """
+    '3H6D1Y2S50M'
+    str = str.lower()
+    
+    lst = []
+    for time in ('d','h','m','s'):
+        
+        index = str.find(time)
+        if index != -1:
+            lst.append({index:time})
+
+    
+        
+
+    
+
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+@client.command()
+async def test(ctx, *args):
+
+    await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+
+
+
+
+
+
+
+
+
+
+client.run(config('TOKEN'))
