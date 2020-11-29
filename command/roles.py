f44b9db56c29ebece153adcdd7278b6c61b8d121
Blitz <55319277+Blitzkriegs03@users.noreply.github.com>
2020-11-29 13:47:38 +0000

    added commands module
    added folders for commands to be imported and set up a general format for importing commands

---
+++ command/roles.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+class persistant_role(commands.Cog):
+    """
+    comands and functions for the persistant roles
+    """
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        """
+        auto runs when members run
+        """
+        pass
+        #await channel.send('Welcome {0.mention}.'.format(member))
+        
+
+    #returns a link to the github
+    @commands.command()
+    async def test2(self, ctx):
+        """
+        sends link to the github repo
+        """
+        pass
+
+
+
+def setup(bot):
+    bot.add_cog(persistant_role(bot))
--- main.py
+++ main.py
@@ -1,36 +1,20 @@
-import discord
-from discord.ext import commands
-
 from decouple import config
 
+import discord
+from discord.ext import commands
+from command import *
 
 bot = commands.Bot(command_prefix = '%')
 
+base.setup(bot)
+roles.setup(bot)
 
-###############################################################################
+##########################################################
 
 #prints when bot has started up
 @bot.event
 async def on_ready():
     print('Bot is ready')
 
-class base_commands(commands.Cog):
-    """Cog description"""
-    #returns a link to the github
-    @commands.command(aliases=['git'])
-    async def github(ctx):
-        """
-        sends link to the github repo
-        """
-        await ctx.send('https://github.com/Blotz/gregory')
 
-    #testing with different arguements
-    @commands.command()
-    async def test(ctx, *args):
-        """
-        dumb test command
-        """
-        await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
-
-bot.add_cog(base_commands())
 bot.run(config('TOKEN'))
