98d6d68061b3bdeb4cdfa4d2d9740ee8acf81ee5
Blitz <55319277+Blitzkriegs03@users.noreply.github.com>
2020-12-06 18:15:05 +0000

    pulling updates from pollv2 branch

---
+++ commands/system.py
@@ -0,0 +1,90 @@
+import discord
+from discord.ext import commands
+
+class system(commands.Cog, name='System commands'):
+    '''
+    system commands
+    '''
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def ping(self, ctx):
+        """
+        : works out latancy of bot
+        """
+        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+
+    # Loading and unloading of cogs for testing
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, cog):
+        '''
+        : loads a catagory of commands
+        '''
+        print(f'loading {cog}...')
+        self.client.load_extension(f'commands.{cog}')
+        await ctx.send(f'successfully loaded {cog}')
+        print('success!')
+
+    @commands.command()
+    @commands.is_owner()
+    async def unload(self, ctx, cog):
+        '''
+        : unloads a catagory of commands
+        '''
+        print(f'unloading {cog}...')
+        self.client.unload_extension(f'commands.{cog}')
+        await ctx.send(f'successfully unloaded {cog}')
+        print('success!')
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, cog):
+        '''
+        : reloads a catagory of commands
+        '''
+        print(f'reloading {cog}...')
+        self.client.unload_extension(f'commands.{cog}')
+        self.client.load_extension(f'commands.{cog}')
+        await ctx.send(f'successfully reloaded {cog}')
+        print('success!')
+
+
+
+    # Loading and unloading of cogs Error handling
+    @load.error
+    async def load_error(self, ctx, error):
+        # error if cog doesnt exist
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('`ERROR: cog has not been loaded`')
+        
+        # error if user is not bot owner/insufficant perms 
+        if isinstance(error, commands.errors.NotOwner):
+            await ctx.send('`ERROR: insufficant perms to run this command`')
+        print('failure!')
+
+    @unload.error
+    async def unload_error(self, ctx, error):
+        # error if cog doesnt exist
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('`ERROR: cog has not been unloaded`')
+        
+        # error if user is not bot owner/insufficant perms 
+        if isinstance(error, commands.errors.NotOwner):
+            await ctx.send('`ERROR: insufficant perms to run this command`')
+        print('failure!')
+
+    @reload.error
+    async def reload_error(self, ctx, error):
+        # error if cog doesnt exist
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send('`ERROR: cog has not been reloaded`')
+        
+        # error if user is not bot owner/insufficant perms 
+        if isinstance(error, commands.errors.NotOwner):
+            await ctx.send('`ERROR: insufficant perms to run this command`')
+        print('failure!')
+
+def setup(client):
+    client.add_cog(system(client))
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from decouple import config
 import os
-
 import discord
 from discord.ext import commands
+from decouple import config
 
 # add discord bot perms
 intents = discord.Intents.default()
@@ -15,83 +14,10 @@ for filename in os.listdir('./commands'):
     if filename.endswith('.py'):
         client.load_extension(f'commands.{filename[:-3]}')
 
-
 # prints when bot has started up
 @client.event
 async def on_ready():
     print('bot done')
-
-# Loading and unloading of cogs for testing
-@client.command()
-@commands.is_owner()
-async def load(ctx, cog):
-    '''
-    : loads a catagory of commands
-    '''
-    print(f'loading {cog}...')
-    client.load_extension(f'commands.{cog}')
-    await ctx.send(f'successfully loaded {cog}')
-    print('success!')
-
-
-@client.command()
-@commands.is_owner()
-async def unload(ctx, cog):
-    '''
-    : unloads a catagory of commands
-    '''
-    print(f'unloading {cog}...')
-    client.unload_extension(f'commands.{cog}')
-    await ctx.send(f'successfully unloaded {cog}')
-    print('success!')
-
-@client.command()
-@commands.is_owner()
-async def reload(ctx, cog):
-    '''
-    : reloads a catagory of commands
-    '''
-    print(f'reloading {cog}...')
-    client.unload_extension(f'commands.{cog}')
-    client.load_extension(f'commands.{cog}')
-    await ctx.send(f'successfully reloaded {cog}')
-    print('success!')
-
-
-# Loading and unloading of cogs Error handling
-@load.error
-async def load_error(ctx, error):
-    # error if cog doesnt exist
-    if isinstance(error, commands.CommandInvokeError):
-        await ctx.send('`ERROR: cog has not been loaded`')
-    
-    # error if user is not bot owner/insufficant perms 
-    if isinstance(error, commands.errors.NotOwner):
-        await ctx.send('`ERROR: insufficant perms to run this command`')
-    print('failure!')
-
-@unload.error
-async def unload_error(ctx, error):
-    # error if cog doesnt exist
-    if isinstance(error, commands.CommandInvokeError):
-        await ctx.send('`ERROR: cog has not been unloaded`')
-    
-    # error if user is not bot owner/insufficant perms 
-    if isinstance(error, commands.errors.NotOwner):
-        await ctx.send('`ERROR: insufficant perms to run this command`')
-    print('failure!')
-
-
-@reload.error
-async def reload_error(ctx, error):
-    # error if cog doesnt exist
-    if isinstance(error, commands.CommandInvokeError):
-        await ctx.send('`ERROR: cog has not been reloaded`')
-    
-    # error if user is not bot owner/insufficant perms 
-    if isinstance(error, commands.errors.NotOwner):
-        await ctx.send('`ERROR: insufficant perms to run this command`')
-    print('failure!')
 
 
 client.run(config('TOKEN'))
