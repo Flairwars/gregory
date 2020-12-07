3a4f546dd15044929b0872a377cbbfc512b37205
Blotz <f.p.theil@gmail.com>
2020-12-07 12:54:58 +0000

    updated file structure

---
+++ commands/base.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class base(commands.Cog, name='Basic Commands'):
+    '''
+    basic commands
+    does simple things which all users have access to
+    '''
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['git'])
+    async def github(ctx):
+        """
+        : sends link to the github repo
+        """
+        await ctx.send('https://github.com/Blotz/gregory')
+    
+
+def setup(client):
+    client.add_cog(base(client))
