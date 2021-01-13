5f6756259f9d3377a94e6664d9ae6cef43bcbacb
Excel <excelthebeanman@gmail.com>
2021-01-13 12:35:29 +0200

    F
    wait

--- src/commands/cat_fun/F.py
+++ src/commands/cat_fun/F.py
@@ -4,7 +4,6 @@ from random import randint
 from discord.ext import commands
 respects_id = 587602797139329064
 st_id = 708108259348709479
-greg_id = 771011111242956820
 
 
 class fInChat(commands.Cog, name='fInChat'):
@@ -17,13 +16,12 @@ class fInChat(commands.Cog, name='fInChat'):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if message.author == self.client.user:
+            return
 
-        if message.content == "F" and (randint(1, 6) == 1) and not (message.author.id in [greg_id, st_id]):
+        if message.content == "F" and (randint(1, 6) == 1 or message.author.id != st_id):
             await message.channel.send("F")
             if randint(1, 17) == 1:
                 await message.channel.send("wait") #F...wait
-
-        if (message.author.id == st_id) and (message.channel.id == respects_id): #responds to ST's Fs
-            await message.channel.send("F")
 
 #I'm only commenting this cause it's good coding practice but honestly a 4 year-old could probably understand this code
