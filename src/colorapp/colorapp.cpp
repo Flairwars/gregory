3b8b1fa43a835fb6205b61b911436b508d5fa5be
rebalint <rebalint@gmail.com>
2021-04-10 01:21:00 +0200

    added cpp file

---
+++ src/colorapp/colorapp.cpp
@@ -0,0 +1,45 @@
+//cppimport
+
+#include "pybind/include/pybind11/pybind11.h"
+#include "pybind/include/pybind11/stl.h"
+#include <vector>
+#include <string>
+#include <cmath>
+#include <typeinfo>
+
+namespace py = pybind11;
+
+py::bytes recolor_fun(std::string img, unsigned int height, unsigned int width, unsigned char r, unsigned char g, unsigned char b, float intensity){
+	//create vector of prescaled target rgb
+	// old algo
+	 //std::vector<unsigned char> rgb = {r*intensity, g*intensity, b*intensity, 0};
+	std::vector<unsigned char> rgb = {r, g, b};
+
+	for(size_t i = 0; i<width*height*4; i++){
+		//DEBUG: PRINT PIXEL VALUE and mod
+		// std::cout << "value for " << i%4 << ": " << static_cast<int>(img[i]) << ", rgbmod: " << static_cast<int>(rgb[i%4]);
+		if(i%4!=3){
+			unsigned char *p = reinterpret_cast<unsigned char*>(&img[i]);
+			// old algo
+			// img[i] = round(*p * (1-intensity) + rgb[i%4]);
+			signed int part = *p + ((signed int) *p * ((float)(rgb[i%4]-*p)/255)*intensity);
+			img[i] = part;
+		}
+		//DEBUG: PRINT PIXEL VALUE after mod
+		// std::cout << ", after mod: " << (int) img[i] << std::endl;
+	}
+	return(img);
+}
+
+PYBIND11_MODULE(colorapp, m){
+    m.doc() = "recolor images in cpp";
+    m.def("recolor", &recolor_fun);
+}
+
+//mako setup for compiling
+/*
+<%
+cfg['dependencies'] = ['pybind/include/pybind11/stl.h', 'pybind/include/pybind11/pybind11.h']
+setup_pybind11(cfg)
+%>
+ */
